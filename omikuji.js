926093f1f1fbc27a8fb3724af21d6e68dc649378
haruto asano <131452108+bskal9221@users.noreply.github.com>
2023-04-25 22:30:31 +0900

    Update omikuji.js

--- omikuji.js
+++ omikuji.js
@@ -1,7 +1,9 @@
 let username;
 let userresult;
 username = prompt("お名前を教えて下さい。");
-document.getElementById("").innerHTML = username;
+if (usernme == "") {userame = "名無し";
+}
+document.getElementById("name").innerHTML = username;
 
 let rand = Math.floor( Math.random() * 5);
 if (rand == 0) {
